6a1365970fae91aec5796341e598c92aa4b2782b
serioushyeon <zz64446949@gmail.com>
2025-04-11 20:02:20 +0900

    [feat] UI 및 로직 구현

---
+++ app/src/main/java/org/sopt/at/feature/signin/SignInScreen.kt
@@ -0,0 +1,146 @@
+package org.sopt.at.feature.signin
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.at.core.component.BackButtonTopBar
+import org.sopt.at.core.component.TivingCommonPasswordField
+import org.sopt.at.core.component.TivingCommonTextField
+import org.sopt.at.core.extension.noRippleClickable
+import org.sopt.at.ui.theme.Black
+import org.sopt.at.ui.theme.Gray1
+import org.sopt.at.ui.theme.Gray2
+import org.sopt.at.ui.theme.Gray4
+import org.sopt.at.ui.theme.TivingPrimary
+import org.sopt.at.ui.theme.White
+
+@Composable
+fun SignInScreen(
+    modifier: Modifier = Modifier,
+    onLoginButtonClick: (String, String) -> Unit = { _, _ ->},
+    onSignUpButtonClick: () -> Unit = {},
+) {
+    var id by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val backgroundColor = if (id.isNotBlank() && password.isNotBlank())
+        TivingPrimary
+    else
+        Gray4
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        BackButtonTopBar(
+            onBackButtonPress = { // TODO: 뒤로가기
+            }
+        )
+        Column(
+            modifier = Modifier
+                .padding(top = 0.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+        ) {
+            Spacer(Modifier.height(20.dp))
+            Text(
+                text = "TIVING ID 로그인",
+                color = White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+            Spacer(Modifier.height(20.dp))
+            TivingCommonTextField(
+                value = id,
+                hint = "아이디",
+            ) {
+                id = it
+            }
+            Spacer(Modifier.height(8.dp))
+            TivingCommonPasswordField(
+                value = password,
+                hint = "비밀번호"
+            ) {
+                password = it
+            }
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = "로그인 하기",
+                color = Gray2,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .imePadding()
+                    .background(
+                        backgroundColor,
+                        RoundedCornerShape(7.dp)
+                    )
+                    .padding(14.dp)
+                    .noRippleClickable {
+                        onLoginButtonClick(id, password)
+                    }
+
+            )
+            Spacer(Modifier.height(20.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .wrapContentHeight(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "아이디 찾기",
+                    color = Gray1,
+                )
+                VerticalDivider(modifier = Modifier.height(16.dp), thickness = 1.dp, color = Gray1)
+                Text(
+                    text = "비밀번호 찾기",
+                    color = Gray1,
+                )
+                VerticalDivider(modifier = Modifier.height(16.dp), thickness = 1.dp, color = Gray1)
+                Text(
+                    text = "회원가입",
+                    color = Gray1,
+                    modifier = Modifier.noRippleClickable(onSignUpButtonClick)
+                )
+            }
+            Spacer(Modifier.height(20.dp))
+            Text(
+                text = "이 사이트는 Google reCAPTCHA로 보호되며,\nGoogle 개인정보 처리 방침과 서비스 약관이 적용됩니다.",
+                fontSize = 12.sp,
+                color = Gray4,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewSignInScreen() {
+    Column(Modifier.background(Black)) {
+        SignInScreen()
+    }
+}
---
+++ app/src/main/java/org/sopt/at/feature/signup/SignUpScreen.kt
@@ -0,0 +1,168 @@
+package org.sopt.at.feature.signup
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.at.core.component.BackButtonTopBar
+import org.sopt.at.core.component.TivingCommonPasswordField
+import org.sopt.at.core.component.TivingCommonTextField
+import org.sopt.at.core.extension.noRippleClickable
+import org.sopt.at.ui.theme.Black
+import org.sopt.at.ui.theme.Gray2
+import org.sopt.at.ui.theme.Gray4
+import org.sopt.at.ui.theme.White
+
+enum class SignUpStep {
+    ID, PASSWORD
+}
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    onNextButtonClick: (String, String) -> Unit = { _, _ -> },
+) {
+    var step by remember { mutableStateOf(SignUpStep.ID) }
+    var id by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val titleText = when (step) {
+        SignUpStep.ID -> "아이디를 입력해주세요."
+        SignUpStep.PASSWORD -> "비밀번호를 입력해주세요."
+    }
+
+    val hintText = when (step) {
+        SignUpStep.ID -> "아이디"
+        SignUpStep.PASSWORD -> "비밀번호"
+    }
+
+    val isNextEnabled = when (step) {
+        SignUpStep.ID -> Regex("^[a-z0-9]{6,12}$").matches(id)
+        SignUpStep.PASSWORD -> Regex("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#\$%^&*])[A-Za-z\\d~!@#\$%^&*]{8,15}\$")
+            .matches(password)
+    }
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        BackButtonTopBar(
+            onBackButtonPress = { // TODO: 뒤로가기
+            }
+        )
+        Column(
+            modifier = Modifier
+                .padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+        ) {
+
+            Text(
+                text = titleText,
+                color = White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            when (step) {
+                SignUpStep.ID -> {
+                    TivingCommonTextField(
+                        value = id,
+                        hint = hintText,
+                        onValueChange = { id = it }
+                    )
+                }
+
+                SignUpStep.PASSWORD -> {
+                    TivingCommonPasswordField(
+                        value = password,
+                        hint = hintText,
+                        onValueChange = { password = it }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = when (step) {
+                    SignUpStep.ID -> "영문 소문자 또는 영문 소문자, 숫자 조합 6~12 자리"
+                    SignUpStep.PASSWORD -> "영문, 숫자, 특수문자(~!@#$%^&*) 조합 8~15 자리"
+                },
+                fontSize = 14.sp,
+                color = Gray4,
+            )
+
+            Spacer(Modifier.weight(1f))
+            Text(
+                text = "다음",
+                color = if (isNextEnabled) Black else Gray2,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .imePadding()
+                    .background(
+                        color = if (isNextEnabled) White else Color.Transparent,
+                        shape = RoundedCornerShape(4.dp)
+                    )
+                    .border(
+                        1.dp,
+                        if (isNextEnabled) Color.Transparent else Gray2,
+                        RoundedCornerShape(4.dp)
+                    )
+                    .padding(16.dp)
+                    .noRippleClickable {
+                        if (!isNextEnabled) {
+                            Toast.makeText(
+                                context,
+                                when (step) {
+                                    SignUpStep.ID -> "아이디 형식이 올바르지 않습니다."
+                                    SignUpStep.PASSWORD -> "비밀번호 형식이 올바르지 않습니다."
+                                },
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            return@noRippleClickable
+                        }
+
+                        when (step) {
+                            SignUpStep.ID -> step = SignUpStep.PASSWORD
+                            SignUpStep.PASSWORD -> onNextButtonClick(id, password)
+                        }
+                    }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewSignUpScreen() {
+    Column(Modifier.background(Black)) {
+        SignUpScreen()
+    }
+}
