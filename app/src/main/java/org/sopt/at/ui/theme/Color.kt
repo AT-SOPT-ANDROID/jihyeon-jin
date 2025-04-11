279b12be0f9894de8da0bf4bb3ca622a35ae0314
serioushyeon <zz64446949@gmail.com>
2025-04-11 20:01:03 +0900

    [add] 컬러 추가

--- app/src/main/java/org/sopt/at/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/at/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val TivingPrimary = Color(0xFFFF153C)
+val TivingBg = Color(0xFF121212)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val Gray = Color(0xFFF0F0F0)
+val Gray1 = Color(0xFFF1F1F1)
+val Gray2 = Color(0xFFDDDDDD)
+val Gray3 = Color(0xFF8B8B8B)
+val Gray4 = Color(0xFF434343)
