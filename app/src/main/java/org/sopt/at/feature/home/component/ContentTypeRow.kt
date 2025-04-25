499848daa8a1f7da32a5ffd421e87d4a167d98b9
serioushyeon <zz64446949@gmail.com>
2025-04-25 23:50:01 +0900

    [feat] ContentTypeRow 구현

---
+++ app/src/main/java/org/sopt/at/feature/home/component/ContentTypeRow.kt
@@ -0,0 +1,40 @@
+package org.sopt.at.feature.home.component
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import org.sopt.at.core.extension.noRippleClickable
+import org.sopt.at.core.type.ContentType
+import org.sopt.at.ui.theme.Gray3
+import org.sopt.at.ui.theme.White
+
+@Composable
+fun ContentTypeRow(
+    selectedContentType: ContentType?,
+    onContentTypeSelected: (ContentType) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.horizontalScroll(rememberScrollState()),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        ContentType.entries.forEach { contentType ->
+            val isSelected = contentType == selectedContentType
+            Text(
+                modifier = Modifier
+                    .noRippleClickable(onClick = { onContentTypeSelected(contentType) }),
+                text = stringResource(contentType.titleResId),
+                fontSize = 18.sp,
+                fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+                color = if (isSelected) White else Gray3
+            )
+        }
+    }
+}
