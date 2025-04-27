33c81558d14187461b4d7f5f361ce411685635f8
serioushyeon <zz64446949@gmail.com>
2025-04-28 03:05:59 +0900

    [fix] 페이저 문제 해결

--- app/src/main/java/org/sopt/at/feature/home/component/BannerPager.kt
+++ app/src/main/java/org/sopt/at/feature/home/component/BannerPager.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,10 +19,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.sopt.at.R
 import org.sopt.at.domain.model.ContentCategory
 import org.sopt.at.domain.model.Program
@@ -34,19 +29,17 @@ fun BannerPager(
     programList: List<Program>,
     modifier: Modifier = Modifier
 ) {
-    val coroutine = rememberCoroutineScope()
-
     val pagerState = rememberPagerState(
         initialPage = Int.MAX_VALUE / 2,
         pageCount = { Int.MAX_VALUE }
     )
-
-    LaunchedEffect(true) {
+    LaunchedEffect(Unit) {
         while (true) {
             delay(3000)
-            coroutine.launch {
-                pagerState.animateScrollToPage(pagerState.currentPage + 1)
-            }
+            pagerState.animateScrollToPage(
+                page = pagerState.currentPage + 1,
+                animationSpec = spring(stiffness = Spring.StiffnessLow)
+            )
         }
     }
 
@@ -69,22 +62,6 @@ fun BannerPager(
                         .clip(RoundedCornerShape(10.dp)),
                     contentScale = ContentScale.FillWidth,
                     alignment = Alignment.BottomCenter
-                )
-            }
-        }
-    }
-    AutoScrollEffect(pagerState)
-}
-
-@Composable
-fun AutoScrollEffect(pagerState: PagerState) {
-    LaunchedEffect(pagerState.currentPage) {
-        while (true) {
-            delay(3000)
-            withContext(NonCancellable) {
-                pagerState.animateScrollToPage(
-                    page = pagerState.currentPage + 1,
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
                 )
             }
         }
