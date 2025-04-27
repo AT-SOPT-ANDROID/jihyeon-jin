86e29280b1825375c753f32d005b83a65bf4d44d
serioushyeon <zz64446949@gmail.com>
2025-04-28 03:22:33 +0900

    [chore] 코드 복구

--- app/src/main/java/org/sopt/at/feature/home/component/BannerPager.kt
+++ app/src/main/java/org/sopt/at/feature/home/component/BannerPager.kt
@@ -44,24 +44,26 @@ fun BannerPager(
     }
 
     HorizontalPager(
-        state = pagerState,
-        beyondViewportPageCount = 1,
-        modifier = modifier.height(450.dp),
-        contentPadding = PaddingValues(horizontal = 24.dp),
-        pageSpacing = 12.dp
+    state = pagerState,
+    beyondViewportPageCount = 1,
+    modifier = modifier.height(450.dp),
+    contentPadding = PaddingValues(horizontal = 24.dp),
+    pageSpacing = 12.dp
     ) { page ->
         Box(
             contentAlignment = Alignment.BottomEnd
         ) {
-            Image(
-                painter = painterResource(programList[page % programList.size].imgRes),
-                contentDescription = programList[page % programList.size].title,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(10.dp)),
-                contentScale = ContentScale.FillWidth,
-                alignment = Alignment.BottomCenter
-            )
+            if (programList.isNotEmpty()) {
+                Image(
+                    painter = painterResource(programList[page % programList.size].imgRes),
+                    contentDescription = programList[page % programList.size].title,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(10.dp)),
+                    contentScale = ContentScale.FillWidth,
+                    alignment = Alignment.BottomCenter
+                )
+            }
         }
     }
 }
